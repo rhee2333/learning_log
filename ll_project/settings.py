4c7a98f9397b99d31268695cf4b9a3fc721ca514
rhee2333 <rhee2333@gmail.com>
2024-04-02 11:35:43 +0800

    新增页面访问限制

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -131,3 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # 自定义设置
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
