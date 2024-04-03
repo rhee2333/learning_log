8c153ebc25033324ab61a1e9d7689be86f6bd1a3
rhee2333 <rhee2333@gmail.com>
2024-04-03 14:25:19 +0800

    定制错误页面

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2!1n5&(=-(_l4ur*gud3s80f^xmprzma%tmq&*q()vx%c%@+n)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'll_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
